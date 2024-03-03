27cebca951fb2eedaf8820299e4e6343b6384189
swapnilb102 <146473025+swapnilb102@users.noreply.github.com>
2024-03-03 19:52:41 +0530

    Create 03_variable.js

---
+++ 03_variable.js
@@ -0,0 +1,15 @@
+//declaring variable in Js
+let a = 7;
+
+//assigning string to a variable
+let name = "Sam"
+
+//creating a variable with boolean datatype
+const a = true;
+const b = false;
+
+//creating a variable with undefined datatype
+let x;
+
+//creating a variable with null datatype
+let c = null;
