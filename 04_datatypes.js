4a9493ab1dd81774693017077ad679958e22b9bf
swapnilb102 <146473025+swapnilb102@users.noreply.github.com>
2024-03-03 20:00:09 +0530

    Create 04_datatypes.js

---
+++ 04_datatypes.js
@@ -0,0 +1,16 @@
+//Using typeof operator to check different datatypes.
+let a = "Sam";
+let b = 46;
+let c = true;
+let d = null;
+let e ;
+console.log(typeof a)
+//output : String
+console.log(typeof b)
+//output : Number
+console.log(typeof c)
+//output : Boolean
+console.log(typeof d)
+//output : Null
+console.log(typeof e)
+//output : Undefined
